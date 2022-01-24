41d47df5bcbbd0571cc1445b1931158e8b31ad2c
Shuvam Das <repository.shuvam@gmail.com>
2022-01-24 19:50:43 +0530

    [Shuvam] Added deck of card program added

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DayElevenAndTwelveAssignment;
+internal class Program
+{
+    public static void Main()
+    {
+        var dc = new DeckOfCards();
+        dc.Solution();
+    }
+}
